11a255b3091a19f3311b0200408faf38670b02d7
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-03-30 17:39:51 +0100

    section 01 completa com styling

--- portifolio/src/components/Section01/index.jsx
+++ portifolio/src/components/Section01/index.jsx
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './style.scss';
 
 export default class Section01 extends Component {
   render() {
     return (
-      <section id="#home">
+      <section id="#home" className="pt-5 pb-5 Home">
         <div className="container">
           <div className="row">
 
@@ -13,12 +14,18 @@ export default class Section01 extends Component {
             </div>{/* /col */}
             
             {/*First COL */}
-            <div className="col-md-7 text-lg-right text-sm-center">
+            <div className="col-md-7 text-lg-right text-sm-center 
+            d-flex justify-content-end align-items-center">
+            
+            <div>
               <h1>High value experiences for all screens</h1>
 
               <h2>I‘m Ana Flavia Foppa Web developer looking for opportunities 
               to learn and create digital experiences 
               that simplify and enhance value for companies.</h2>
+
+            </div>
+              
 
             </div>{/* /col */}
 
